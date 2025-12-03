9815b7167ceea41f31838ed15cbcee1b094c1ec1
zer0p01nt <1027.bmy@gmail.com>
2025-12-04 04:02:27 +0900

    ✨ Feat: 친구 및 그룹 페이지 검색 화면 구현 (연동 X)

--- src/pages/FriendGroup/FriendGroup.tsx
+++ src/pages/FriendGroup/FriendGroup.tsx
@@ -8,6 +8,8 @@ import ConfirmModal from "@/components/ConfirmModal/ConfirmModal";
 import ToastModal from "@/components/ToastModal/ToastModal";
 
 export default function FriendGroup() {
+  const [keyword, setKeyword] = useState("");
+
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [showDeleteToast, setShowDeleteToast] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState(""); // 삭제할 친구 이름 상태
@@ -86,10 +88,12 @@ export default function FriendGroup() {
         <Search
           placeholder="검색"
           desc="이메일, 닉네임, 그룹명으로 계정을 검색할 수 있어요."
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
         />
         <S.Section>
           <S.Title>
-            <div>나의 그룹</div>
+            <div>{keyword ? "그룹" : "나의 그룹"}</div>
             <S.BtnBox>
               <button onClick={() => navigate("/create-group")}>
                 그룹 만들기
@@ -98,73 +102,54 @@ export default function FriendGroup() {
             </S.BtnBox>
           </S.Title>
           <S.GroupContainer>
-            {/* 가입한 그룹 배열 받아서 매핑 */}
-            <S.GroupBox onClick={() => navigate("/group")}>
-              <S.GroupName>무니니</S.GroupName>
-              <S.GroupCount>3</S.GroupCount>
-            </S.GroupBox>
-            {/* 검색 결과 나온 그룹 리스트 => join-group url 바뀌면 같이 수정 */}
-            <S.GroupBox onClick={() => navigate("/join-group")}>
-              <S.GroupName>스어 친구들</S.GroupName>
-              <S.GroupCount>2</S.GroupCount>
-            </S.GroupBox>
+            {keyword ? (
+              <S.GroupBox onClick={() => navigate("/join-group")}>
+                {/* 검색 결과 나온 그룹 리스트 => join-group url 바뀌면 같이 수정 */}
+                <S.GroupName>스어 친구들</S.GroupName>
+                <S.GroupCount>2</S.GroupCount>
+              </S.GroupBox>
+            ) : (
+              <>
+                <S.GroupBox onClick={() => navigate("/group")}>
+                  {/* 가입한 그룹 배열 받아서 매핑 */}
+                  <S.GroupName>무니니</S.GroupName>
+                  <S.GroupCount>3</S.GroupCount>
+                </S.GroupBox>
+                <S.GroupBox onClick={() => navigate("/group")}>
+                  {/* 가입한 그룹 배열 받아서 매핑 */}
+                  <S.GroupName>무니니</S.GroupName>
+                  <S.GroupCount>3</S.GroupCount>
+                </S.GroupBox>
+              </>
+            )}
           </S.GroupContainer>
         </S.Section>
         <S.Section>
-          <S.Title>친구</S.Title>
+          {!keyword && <S.Title>친구</S.Title>}
           <S.FriendList>
-            {/* 친구 리스트 배열 받아서 매핑 */}
-            <List
-              profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
-              username="듀랄라"
-              intro="Positive Thinking"
-              actionButton={{
-                type: "delete",
-                text: "삭제",
-                onClick: () => handleDeleteFriend("듀랄라"),
-              }}
-            />
-            {/* 검색 결과 나온 유저 리스트 */}
-            <List
-              profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
-              username="어푸"
-              intro="작심삼일의 권위자"
-              actionButton={{
-                type: "add",
-                text: "추가",
-                onClick: () => handleAddFriend("어푸"),
-              }}
-            />
-            <List
-              profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
-              username="어푸"
-              intro="작심삼일의 권위자"
-              actionButton={{
-                type: "add",
-                text: "추가",
-                onClick: () => handleAddFriend("어푸"),
-              }}
-            />
-            <List
-              profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
-              username="어푸"
-              intro="작심삼일의 권위자"
-              actionButton={{
-                type: "add",
-                text: "추가",
-                onClick: () => handleAddFriend("어푸"),
-              }}
-            />
-            <List
-              profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
-              username="어푸"
-              intro="작심삼일의 권위자"
-              actionButton={{
-                type: "add",
-                text: "추가",
-                onClick: () => handleAddFriend("어푸"),
-              }}
-            />
+            {keyword ? (
+              <List
+                profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
+                username="어푸"
+                intro="작심삼일의 권위자"
+                actionButton={{
+                  type: "add",
+                  text: "추가",
+                  onClick: () => handleAddFriend("어푸"),
+                }}
+              />
+            ) : (
+              <List
+                profileImgUrl="https://avatars.githubusercontent.com/u/189887138?v=4"
+                username="듀랄라"
+                intro="Positive Thinking"
+                actionButton={{
+                  type: "delete",
+                  text: "삭제",
+                  onClick: () => handleDeleteFriend("듀랄라"),
+                }}
+              />
+            )}
           </S.FriendList>
         </S.Section>
       </S.Content>
