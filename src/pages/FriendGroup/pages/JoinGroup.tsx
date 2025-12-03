94d6bfad9d4271433587dcf30f4013c3bd029fb8
zer0p01nt <1027.bmy@gmail.com>
2025-12-04 03:04:42 +0900

    ✨ Feat: 그룹 가입 및 나의 그룹 관리 페이지 모달 포함 퍼블리싱

--- src/pages/FriendGroup/pages/JoinGroup.tsx
+++ src/pages/FriendGroup/pages/JoinGroup.tsx
@@ -1,11 +1,41 @@
 import Header from "@/components/Header/Header";
 import * as S from "./PagesStyle";
 import { useNavigate } from "react-router-dom";
+import GroupCard from "@/pages/FriendGroup/components/GroupCard";
+import { useCallback, useState } from "react";
+import ToastModal from "@/components/ToastModal/ToastModal";
+import ConfirmModal from "@/components/ConfirmModal/ConfirmModal";
 
 export default function JoinGroup() {
+  // TO-DO : useParams() 활용해 그룹 ID 받아온 뒤, 해당 그룹 정보 API로 조회해서 렌더링
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+
+  // 그룹 참여 요청 전송 로직
+  const handleConfirm = useCallback(() => {
+    setShowModal(false);
+    setShowToast(true);
+  }, []);
+
   return (
     <S.Container>
+      {showModal && (
+        <ConfirmModal
+          question="그룹 참여를 요청할까요?"
+          onClose={() => setShowModal(false)}
+          onConfirm={handleConfirm}
+          showOverlay={false}
+        />
+      )}
+      {showToast && (
+        <ToastModal
+          text="그룹 참여를 요청했습니다."
+          isVisible={showToast}
+          onClose={() => setShowToast(false)}
+          showOverlay={false}
+        />
+      )}
       <Header
         showBackBtn={true}
         showXBtn={false}
@@ -13,6 +43,14 @@ export default function JoinGroup() {
         backgroundColor="white"
         onClickBackBtn={() => navigate(-1)}
       />
+      <S.Content>
+        <GroupCard
+          title="스어 친구들"
+          memberCount={3}
+          sinceYear={2025}
+          onBtnClick={() => setShowModal(true)}
+        />
+      </S.Content>
     </S.Container>
   );
 }
