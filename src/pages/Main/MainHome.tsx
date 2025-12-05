8b59635ec63d9ab136b8bf444d6437244733eafa
zer0p01nt <1027.bmy@gmail.com>
2025-12-05 15:54:27 +0900

    🐛 Fix: 토글 친구 및 그룹 버튼 라우팅 추가

--- src/pages/Main/MainHome.tsx
+++ src/pages/Main/MainHome.tsx
@@ -5,24 +5,29 @@ import * as S from "@/pages/Main/MainStyled";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default function MainHome() {
-    const navigate = useNavigate();
-    const [active, setActive] = useState(false); 
+  const navigate = useNavigate();
+  const [active, setActive] = useState(false);
 
-    return (
-        <S.Container>
+  return (
+    <S.Container>
+      <DateHeader active={active} setActive={setActive} />
 
-            <DateHeader active={active} setActive={setActive} />
+      {active && (
+        <S.Toggle>
+          <S.ToggleBtn onClick={() => navigate("/friend-group")}>
+            친구 및 그룹
+          </S.ToggleBtn>
+          <S.ToggleBtn onClick={() => navigate("/profile-centerX")}>
+            프로필 관리
+          </S.ToggleBtn>
+          <S.ToggleBtn onClick={() => navigate("/setting-page")}>
+            환경 설정
+          </S.ToggleBtn>
+        </S.Toggle>
+      )}
 
-            {active && 
-                <S.Toggle>
-                    <S.ToggleBtn onClick={() => navigate("")}>친구 및 그룹</S.ToggleBtn>
-                    <S.ToggleBtn onClick={() => navigate("/profile-centerX")}>프로필 관리</S.ToggleBtn>
-                    <S.ToggleBtn onClick={() => navigate("/setting-page")}>환경 설정</S.ToggleBtn>
-                </S.Toggle>
-            }
-
-            <MainHomeBox />
-            <FeedList />
-        </S.Container>
-    );
+      <MainHomeBox />
+      <FeedList />
+    </S.Container>
+  );
 }
