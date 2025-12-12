adcd2056319958d16d79ffda272e31f318d94c54
zer0p01nt <1027.bmy@gmail.com>
2025-12-13 04:59:26 +0900

    🐛 Fix: 나의 명언 컴포넌트 prop 수정 및 목데이터 나의 명언 페이지에 적용

--- src/data/archiveFeeds.ts
+++ src/data/archiveFeeds.ts
@@ -4,6 +4,7 @@ export const MOCK_ARCHIVE_FEEDS: ArchiveFeed[] = [
   {
     id: 1,
     content: "방귀 뀐 놈이 성낸다",
+    // 30자 초과해 원본 컨텐츠도 함께 제공
     originalContent:
       "오늘 말랑이랑 몰랑이랑 같이 카공을 했는데 갑자기 냄새가 났다. 갑자기 말랑이가 나를 의심하기 시작했고, 당황한 나는 어쩌구저쩌구 여기부터는 안 보이도록 합니당",
     createDate: "2025-12-13T21:52:40.724895",
@@ -13,7 +14,6 @@ export const MOCK_ARCHIVE_FEEDS: ArchiveFeed[] = [
   {
     id: 2,
     content: "방귀 뀐 놈이 성낸다",
-    originalContent: "",
     createDate: "2025-12-13T21:52:40.724895",
     authorName: "듀가나다",
     taggedMemberNames: ["말랑이", "몰랑이"],
@@ -21,7 +21,6 @@ export const MOCK_ARCHIVE_FEEDS: ArchiveFeed[] = [
   {
     id: 3,
     content: "오늘 못한 건 내일의 에너지로 남는다",
-    originalContent: "",
     createDate: "2025-12-13T21:52:40.724895",
     authorName: "듀랄라",
     taggedMemberNames: ["말랑이", "몰랑이"],
--- src/pages/Archive/MyQuotes/MyQuotes.tsx
+++ src/pages/Archive/MyQuotes/MyQuotes.tsx
@@ -2,41 +2,7 @@ import MyQuoteFeed from "@/pages/Archive/MyQuotes/MyQuoteFeed/MyQuoteFeed";
 import * as S from "./MyQuotesStyle";
 import { useCallback, useState } from "react";
 import ConfirmModal from "@/components/ConfirmModal/ConfirmModal";
-
-const mockData = [
-  {
-    date: "10/31",
-    day: "화요일",
-    title: "방귀 뀐 놈이 성 낸다",
-    desc: "오늘 말랑이랑 몰랑이랑 같이 카공을 했는데 갑자기 냄새가 났다. 갑자기 말랑이가 나를 의심하기 시작했고, 당황한 나는 어쩌구저쩌구 여기부턴 말줄임표",
-    tag: ["말랑이", "몰랑이"],
-  },
-  {
-    date: "10/30",
-    day: "월요일",
-    title: "여긴일부러30자거의딱맞춰서줄바꿈보려고길게써볼게요길다길어",
-    desc: "",
-    tag: [], // 일부러 태그 없이
-  },
-  {
-    date: "10/29",
-    day: "일요일",
-    title: "어쩌구저쩌구",
-    desc: "오늘은 완전 정말 대박 진짜 쩔게 어쩌구저쩌구였다 울라불라숑",
-    tag: [
-      "말랑이",
-      "몰랑이",
-      "말랑이",
-      "몰랑이",
-      "말랑이",
-      "몰랑이",
-      "말랑이",
-      "몰랑이",
-      "말랑이",
-      "몰랑이",
-    ],
-  },
-];
+import { MOCK_ARCHIVE_FEEDS } from "@/data/archiveFeeds";
 
 export default function MyQuotes() {
   const [showModal, setShowModal] = useState(false);
@@ -73,15 +39,11 @@ export default function MyQuotes() {
           showOverlay={true}
         />
       )}
-      {mockData.map((data, index) => (
+      {MOCK_ARCHIVE_FEEDS.map((feed) => (
         <MyQuoteFeed
-          key={index}
-          date={data.date}
-          day={data.day}
-          title={data.title}
-          desc={data.desc}
-          tag={data.tag}
-          onClick={() => handleQuoteClick(data.date)}
+          key={feed.id}
+          archiveFeed={feed}
+          onClick={() => handleQuoteClick(feed.createDate)}
         />
       ))}
     </S.Container>
