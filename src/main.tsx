2ec1ec6628d2405e563f8de875a5c153a95d0103
zer0p01nt <1027.bmy@gmail.com>
2026-08-09 19:10:11 +0900

    🔧 Chore: MSW 테스트 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+  return worker.start({
+    onUnhandledRequest: "bypass", // 정의 안 한 백엔드 요청은 그냥 통과
+  });
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
