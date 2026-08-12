7c54261bac0f55da0ffb692f9d24dc7cd27fa4fe
zer0p01nt <1027.bmy@gmail.com>
2026-08-13 01:21:32 +0900

    ♻️ Refactor: Group 스타일 의존성 분리

--- src/pages/Group/Group.styles.ts
+++ src/pages/Group/Group.styles.ts
@@ -1,6 +1,5 @@
 import theme from "@/styles/theme";
 import styled from "@emotion/styled";
-import * as G from "@/pages/FriendGroup/components/GroupCard.styles";
 
 export const Container = styled.div`
   display: flex;
@@ -28,26 +27,67 @@ export const GrayBox = styled.div`
 `;
 
 /* GroupCard 컴포넌트 오버라이딩 */
-export const GroupCard = styled(G.GroupBox)`
+export const GroupCard = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #fff;
   padding: 1.25rem 0.625rem; /* 20px 10px */
   border-top: 0.5px solid #000;
   border-bottom: 0.5px solid #000;
 `;
 
-export const TextBox = styled(G.TextBox)``;
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem; /* 20px */
+`;
 
-export const Title = styled(G.Title)``;
+export const Title = styled.div`
+  ${theme.fonts.batang}
+  color: #000;
+  font-size: 1rem;
+  font-weight: 700;
+  letter-spacing: -0.32px;
+`;
 
-export const InfoBox = styled(G.InfoBox)``;
+export const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem; /* 10px */
+`;
 
-export const InfoLine = styled(G.InfoLine)``;
+export const InfoLine = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3125rem; /* 5px */
+
+  ${theme.fonts.dotum}
+  color: #000;
+  font-size: 0.875rem; /* 14px */
+  letter-spacing: -0.28px;
+
+  div:first-of-type {
+    font-weight: 500;
+  }
+
+  div:last-of-type {
+    font-weight: 300;
+  }
+`;
 
 export const Chonburi = styled.div`
   ${theme.fonts.chonburi}
 `;
 
-export const Count = styled(G.Count)``;
+export const Count = styled.div`
+  ${theme.fonts.chonburi}
+  color: #ddd;
+  font-size: 6rem; /* 96px */
+  font-weight: 400;
+  letter-spacing: -1.92px;
+`;
 
 export const Main = styled(Content)`
   padding: 1rem 2.5rem 1.875rem; /* 30px 40px */
